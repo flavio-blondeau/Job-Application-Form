ad2ef49079973e3acfefb4bd5393705a5ccf6f1b
Flavio <flavioblondeau94@gmail.com>
2023-05-13 17:21:58 +0200

    Add email notification

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, request, flash
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "SevPy&90SecretKey"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "flaviopython@gmail.com"
+app.config["MAIL_PASSWORD"] = "lhrwabkbagnlunrm"
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -32,6 +42,24 @@ def index():
                     date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"""
+        Thank you for your submission, {first_name}.
+        Here are the data you send:
+         - Name: {first_name} {last_name}
+         - Email: {email}
+         - Available date: {date}
+         - Occupation: {occupation}
+        See you soon!
+        
+        The FlavioPython Team
+        """
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash(f"{first_name}, your form has been submitted successfully!", "success")
 
     return render_template("index.html")
